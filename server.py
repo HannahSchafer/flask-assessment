1f3128ee06ff3417d2d979f83c50f1c832fa3e4e
vagrant <vagrant@vagrant.vm>
2017-03-11 19:15:18 +0000

    added route to homepage, with link to application form.

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, request, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+@app.route("/")
+def start_here():
+    """Homepage"""
+
+    return render_template("index.html")
+
+
+@app.route("/application")
+def application_form():
+    """Go to application form."""
+
+    return render_template("application-form.html")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
