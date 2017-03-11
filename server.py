bdeb8f8af2ceff2a140e97cca386941bb8757a1c
vagrant <vagrant@vagrant.vm>
2017-03-11 20:15:11 +0000

    changed jobs to be added with jinja

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, render_template
 from flask_debugtoolbar import DebugToolbarExtension
-
+from random import sample
 
 app = Flask(__name__)
 
 
 # Required to use Flask sessions and the debug toolbar
 app.secret_key = "ABC"
+
+
 
 
 @app.route("/")
@@ -16,11 +18,13 @@ def start_here():
     return render_template("index.html")
 
 
-@app.route("/application")
+@app.route("/application-form")
 def application_form():
     """Go to application form."""
 
-    return render_template("application-form.html")
+    jobs = ["Software Engineer", "QA Engineer", "Product Manager"]
+    
+    return render_template("application-form.html", jobs=jobs)
 
 
 if __name__ == "__main__":
